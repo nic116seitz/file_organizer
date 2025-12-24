41d83ba013a49f6695dd018c31f8552ce07c6b67
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-12-24 09:12:57 -0700

    Tagger Tagger function completed

--- name_checker.py
+++ name_checker.py
@@ -3,7 +3,7 @@ test = "test.jpg"
 # Name checker section
 class File:
    
-    tags = ""
+    tags = []
     ext = ""
 
     def __init__(self, name):
@@ -13,25 +13,43 @@ def name_splitter (filename_comp):
     if "." not in filename_comp:
         file = File(filename)
         file.ext = "none"
-        return [file.name, file.ext]
+        return [ file, file.ext ]
     else:    
         name_ext = filename_comp.split(".")
         name = name_ext[0]
         ext = name_ext[1]
         file = File(name)
-        return [file.name, file.ext]
+        file.ext = ext
+        return [ file, ext ]
 
-def tagger(filename, ext):
-    if "-" in filename:
-        filename.tags = (filename[:-filename.index("-")]).split(" ")
+def tagger(file, ext):
+    if "-" in file.name:
+        filename_tags = (file.name[:-file.name.index("-")]).split(" ")
+        file.tags = filename_tags[0]
+        file.name = filename_tags[1]
     else:
         if ext == "none":
-            filename.tags = "untagged"
+            file.tags.append("untagged") 
             return [filename, filename.tags, ext]
+        # Image sorter
+        elif (ext == "jpg" or 
+            ext ==  "png" or 
+            ext == "tiff" or 
+            ext == "tif" or 
+            ext == "webp" or
+            ext == "gif"):
+            file.tags.append("image")
+            return [file.name, file.tags, ext]
+        elif (ext == "mp3" or 
+              ext == "wav"):
+            file.tags.append("audio")
+            return [file.name, file.tags, ext]
 
-        
 
+
+split_name = name_splitter(test)[0]       
+split_ext = name_splitter(test)[1]
+print(tagger(split_name, split_ext))
 # Debug
-print(name_splitter(test))
 
         
