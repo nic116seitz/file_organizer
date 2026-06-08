d8212f10efb63cdc23683605761bac7e453ae267
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2026-06-08 11:46:12 -0600

    Main splitter written

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import subprocess
-scanDownloads = subprocess.run('ls', '~/Downloads/')
+import os
+
+downloads_path = os.path.expanduser('~/Downloads/')
+scan_downloads = subprocess.run(['ls', '-l', downloads_path], capture_output=True, text=True)
+downloads_array = scan_downloads.stdout.splitlines()
+
+for file in downloads_array:
+    file = file[53:]
+    print(file)
