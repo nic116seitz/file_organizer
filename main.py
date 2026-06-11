731ec023bbe828c00853f491a01d38dfd147a86b
FedMac <25190512+nic116seitz@users.noreply.github.com>
2026-06-10 19:34:41 -0700

    Changed command for better utility

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import subprocess
 import os
 
-downloads_path = os.path.expanduser('~/Downloads/')
-scan_downloads = subprocess.run(['ls', '-l', downloads_path], capture_output=True, text=True)
+downloads_path = os.path.expanduser('~/Documents/Coding')
+scan_downloads = subprocess.run(['ls', downloads_path], capture_output=True, text=True)
 downloads_array = scan_downloads.stdout.splitlines()
 
 for file in downloads_array:
-    file = file[53:]
     print(file)
